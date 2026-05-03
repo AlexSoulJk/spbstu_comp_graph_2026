9d52ec230b72ffe41de56ba08297f9c8228f0396
AlexSoulJk <alex.solomatow2016@yandex.ru>
2026-05-03 21:08:07 +0300

    Start Lab5

---
+++ Lab5/Lab5/Cube.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "ModelManagerAbstract.h"
+class Cube : public ModelManagerAbstract {
+private:
+    float m_CubeAngle;
+public:
+    Cube(ID3D11DeviceContext* context) : ModelManagerAbstract(context), m_CubeAngle() {}
+    ~Cube() override = default;
+    void Update(float dt) override;
+    void Render() override;
+    HRESULT InitModel(ID3D11Device* device);
+};
---
+++ Lab5/Lab5/LightBase.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <DirectXMath.h>
+#include <d3d11.h>
+#include <string>
+using namespace DirectX;
+
+struct PointLightGPU
+{
+    XMFLOAT3 Position;
+    float Range;
+    XMFLOAT3 Color;
+    float Intensity;
+};
+
+class LightBase
+{
+public:
+    virtual ~LightBase() = default;
+
+    void SetName(const std::wstring& n) { m_name = n; }
+    const std::wstring& GetName() const { return m_name; }
+
+    void SetColor(const XMFLOAT3& c) { m_color = c; }
+    const XMFLOAT3& GetColor() const { return m_color; }
+
+    virtual void Fill(PointLightGPU& out) const = 0;
+
+protected:
+    std::wstring m_name = L"Light";
+    XMFLOAT3 m_color = { 1.0f, 1.0f, 1.0f };
+};
